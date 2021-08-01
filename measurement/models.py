d9fc4b27e9cf9e103f23de5a81280d709a0b3266
RajrupDasid <rajrupdasid@outlook.com>
2021-08-01 15:13:58 +0530

    created model field and makes some necessary changes to these field

--- measurement/models.py
+++ measurement/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Measurement(models.Model):
+    location=models.CharField(max_length=1000)
+    destination=models.CharField(max_length=255)
+    distance=models.DecimalField(max_digits=10,decimal_places=2)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __Str__(self):
+        return f"Distance from {self.location} to {self.destination} is {self.distance} km"
