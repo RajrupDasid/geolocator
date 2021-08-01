b6e6907349151b9009ab62f77c64f7a2769088bd
RajrupDasid <rajrupdasid@outlook.com>
2021-08-02 01:05:27 +0530

    django crispy forms hasbeen added

--- geolocator/settings.py
+++ geolocator/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'measurement',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+CRISPY_TEMPLATE_PACK='bootstrap4'
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
--- measurement/views.py
+++ measurement/views.py
@@ -10,7 +10,7 @@ def calculate_distance_view(request):
         instance=form.save(commit=False)
         instance.destination=form.cleaned_data.get('destination')
         instance.location='San Francisco'
-        instance.location=4000.00
+        instance.distance=4000.00
         instance.save()
     context={
         'distance':obj,
