51a8623dde42320ca52f4be66dc1395b32c26a1a
RajrupDasid <rajrupdasid@outlook.com>
2021-08-01 17:08:35 +0530

    bug in views.py file

--- measurement/views.py
+++ measurement/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Measurement
+from .forms import MeasurementModelForm
 # Createfr your views here.
 
 def calculate_distance_view(request):
     obj=get_object_or_404(Measurement,id=1)
+    form=MeasurementModelForm(request.POST or None)
+    if form.is_valid():
+        instance=form.save(commit=False)
+        instance.destination=form.cleaned_data.get('destination')
+        instance.location='San Francisco'
+        instance.location=5000.00
+        instance.save()
     context={
         'distance':obj,
+        'form':form,
     }
     return render(request,'measurements/main.html',context)
